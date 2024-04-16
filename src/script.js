f51b6b7284922762e5a44bea64f40527a53b5b19
Emmanuel <emmanuelokello294@gmail.com>
2024-04-16 23:03:10 +0300

    removed duplicate function

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = ''; 
+
     function getRandomSearchQuery() {
         const words = ['programming', 'tech', 'football', 'music', 'basketball'];
         return words[Math.floor(Math.random() * words.length)];
     }
-    async function fetchRandomYouTubeVideos() {
-        const searchQuery = getRandomSearchQuery();
-        const apiKey = ''; 
+
+    async function fetchYouTubeVideos(searchQuery, targetElementId) {
         const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=${searchQuery}&key=${apiKey}`;
 
         try {
@@ -13,12 +14,13 @@ document.addEventListener('DOMContentLoaded', function() {
             const data = await response.json();
             const videos = data.items;
 
-            const randomVideosDiv = document.querySelector('#randoms-list');
+            const targetElement = document.querySelector(targetElementId);
+            targetElement.innerHTML = '';
 
             videos.forEach(video => {
                 const listItem = document.createElement('li');
                 const videoElement = document.createElement('img');
-                videoElement.src = video.snippet.thumbnails.medium.url; 
+                videoElement.src = video.snippet.thumbnails.medium.url;
                 videoElement.alt = video.snippet.title;
                 videoElement.width = 320;
                 videoElement.height = 240;
@@ -33,51 +35,14 @@ document.addEventListener('DOMContentLoaded', function() {
                     playVideoInIframe(video.id.videoId);
                 });
 
-                randomVideosDiv.appendChild(listItem);
-
-            });
-
-        } catch (error) {
-            console.error('Error fetching random YouTube videos:', error);
-        }
-    }
-    async function fetchYouTubeVideos(searchQuery) {
-        const apiKey = '';
-        const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=${searchQuery}&key=${apiKey}`;
-
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            const videos = data.items;
-
-            const videosList = document.querySelector('#randoms-list');
-            videosList.innerHTML = '';
-
-            videos.forEach(video => {
-                const listItem = document.createElement('li');
-                const videoElement = document.createElement('img');
-                videoElement.src = video.snippet.thumbnails.medium.url; 
-                videoElement.alt = video.snippet.title;
-                videoElement.width = 320;
-                videoElement.height = 240;
-
-                const titleElement = document.createElement('p');
-                titleElement.textContent = video.snippet.title;
-
-                listItem.appendChild(videoElement);
-                listItem.appendChild(titleElement);
-
-                listItem.addEventListener('click', function() {
-                    playVideoInIframe(video.id.videoId);
-                });
-                
-                videosList.appendChild(listItem);
+                targetElement.appendChild(listItem);
             });
 
         } catch (error) {
             console.error('Error fetching YouTube videos:', error);
         }
     }
+
     function playVideoInIframe(videoId) {
         var iframe = document.createElement('iframe');
         iframe.width = '640';
@@ -88,15 +53,16 @@ document.addEventListener('DOMContentLoaded', function() {
         iframe.allowFullscreen = true;
 
         var playerContainer = document.querySelector('.youtube-player');
-        playerContainer.innerHTML = ''; 
+        playerContainer.innerHTML = '';
         playerContainer.appendChild(iframe);
     }
+
     document.getElementById('search-form').addEventListener('submit', function(event) {
-        event.preventDefault(); 
+        event.preventDefault();
         const searchQuery = document.querySelector('#search-form input[type="text"]').value;
-        fetchYouTubeVideos(searchQuery);
-        this.reset()
+        fetchYouTubeVideos(searchQuery, '#randoms-list');
+        this.reset();
     });
 
-    fetchRandomYouTubeVideos();
+    fetchYouTubeVideos(getRandomSearchQuery(), '#randoms-list');
 });
